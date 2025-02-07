42dce8cfcf1ee5e1bdd6340875f2164907d03441
kr37577 <jiatengl127@gmail.com>
2025-02-08 04:07:17 +0900

    sample

--- backend/app.py
+++ backend/app.py
@@ -3,9 +3,19 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
+
 @app.route("/")
 def hello():
     return jsonify({"message": "Hello, Flask + SQLite + Docker!"})
 
+
+
+def main():
+    print("Hello World")
+
+
+
 if __name__ == "__main__":
+    main()
     app.run(host="0.0.0.0", port=5000)
