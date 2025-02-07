6f174697b4166d7ea4af2ea633774c9df23aee46
kr37577 <jiatengl127@gmail.com>
2025-02-08 04:16:53 +0900

    CIの変更

--- backend/app.py
+++ backend/app.py
@@ -2,12 +2,15 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     return jsonify({"message": "Hello, Flask + SQLite + Docker!"})
 
+
 def main():
     print("Hello World")
+
 
 if __name__ == "__main__":
     main()
