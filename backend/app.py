d2ba2b1b79050d8ec17242f68c013f9e5d03bf52
kr37577 <jiatengl127@gmail.com>
2025-02-08 04:08:41 +0900

    delete blank

--- backend/app.py
+++ backend/app.py
@@ -1,20 +1,13 @@
-# backend/app.py
 from flask import Flask, jsonify
 
 app = Flask(__name__)
-
-
 
 @app.route("/")
 def hello():
     return jsonify({"message": "Hello, Flask + SQLite + Docker!"})
 
-
-
 def main():
     print("Hello World")
-
-
 
 if __name__ == "__main__":
     main()
